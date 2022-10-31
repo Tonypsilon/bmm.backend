6203dd56618358caaed8db81da23fbbd28f6cfb7
Tonypsilon <tony.schwedek@posteo.de>
2022-10-31 18:54:21 +0100

    Create TeamCaptainServiceTest.

---
+++ src/test/java/de/tonypsilon/bmm/backend/teamcaptain/service/TeamCaptainServiceTest.java
@@ -0,0 +1,72 @@
+package de.tonypsilon.bmm.backend.teamcaptain.service;
+
+import de.tonypsilon.bmm.backend.exception.AlreadyExistsException;
+import de.tonypsilon.bmm.backend.exception.NotFoundException;
+import de.tonypsilon.bmm.backend.team.service.TeamService;
+import de.tonypsilon.bmm.backend.teamcaptain.data.*;
+import de.tonypsilon.bmm.backend.validation.service.ValidationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class TeamCaptainServiceTest {
+
+    private final TeamCaptainRepository teamCaptainRepository = mock(TeamCaptainRepository.class);
+    private final TeamService teamService = mock(TeamService.class);
+    private final ValidationService validationService = new ValidationService();
+    private TeamCaptainService teamCaptainService;
+    private final TeamCaptainData teamCaptainData1 = new TeamCaptainData(
+            1L, 2L, "team@admin.com", "01234567", "Team", "Admin");
+    private TeamCaptain teamCaptain1;
+
+    @BeforeEach
+    void setUp() {
+        this.teamCaptainService = new TeamCaptainService(teamCaptainRepository,
+                teamService,
+                validationService);
+        teamCaptain1 = new TeamCaptain();
+        teamCaptain1.setId(1L);
+        teamCaptain1.setTeamId(2L);
+        teamCaptain1.setEmailAddress("team@admin.com");
+        teamCaptain1.setPhoneNumber("01234567");
+        teamCaptain1.setForename("Team");
+        teamCaptain1.setSurname("Admin");
+    }
+
+    @Test
+    void testCreateTeamCaptainOk() {
+        CreateTeamCaptainData createTeamCaptainData = new CreateTeamCaptainData(
+                2L, "team@admin.com", "01234567", "Team", "Admin");
+        when(teamService.existsById(2L)).thenReturn(Boolean.TRUE);
+        when(teamCaptainRepository.existsByTeamId(2L)).thenReturn(Boolean.FALSE);
+        when(teamCaptainRepository.getByTeamId(2L)).thenReturn(teamCaptain1);
+        TeamCaptainData actual = teamCaptainService.createTeamCaptain(createTeamCaptainData);
+        assertEquals(teamCaptainData1, actual);
+    }
+
+    @Test
+    void testCreateTeamCaptainTeamDoesNotExist() {
+        CreateTeamCaptainData createTeamCaptainData = new CreateTeamCaptainData(
+                1L, "team@admin.com", "01234567", "Team", "Admin");
+        when(teamService.existsById(1L)).thenReturn(Boolean.FALSE);
+        NotFoundException actualException = assertThrows(NotFoundException.class,
+                () -> teamCaptainService.createTeamCaptain(createTeamCaptainData));
+        assertEquals("Es gibt keine Mannschaft mit der ID 1!", actualException.getMessage());
+    }
+
+    @Test
+    void testCreateTeamCaptainTeamAlreadyHasCaptain() {
+        CreateTeamCaptainData createTeamCaptainData = new CreateTeamCaptainData(
+                2L, "team@admin.com", "01234567", "Team", "Admin");
+        when(teamService.existsById(2L)).thenReturn(Boolean.TRUE);
+        when(teamCaptainRepository.existsByTeamId(2L)).thenReturn(Boolean.TRUE);
+        AlreadyExistsException actualException = assertThrows(AlreadyExistsException.class,
+                () -> teamCaptainService.createTeamCaptain(createTeamCaptainData));
+        assertEquals("Es gibt bereits einen Mannschaftsleiter für die Mannschaft mit der ID 2!",
+                actualException.getMessage());
+    }
+
+}
