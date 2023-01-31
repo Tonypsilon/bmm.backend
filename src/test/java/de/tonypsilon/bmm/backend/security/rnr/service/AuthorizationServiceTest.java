5aa5fefd82e37cd144ed30fbad95ba802f74b61d
Tony <tony.schwedek@posteo.de>
2023-01-31 23:23:41 +0100

    Finish AuthorizationServiceTest.

--- src/test/java/de/tonypsilon/bmm/backend/security/rnr/service/AuthorizationServiceTest.java
+++ src/test/java/de/tonypsilon/bmm/backend/security/rnr/service/AuthorizationServiceTest.java
@@ -1,5 +1,6 @@
 package de.tonypsilon.bmm.backend.security.rnr.service;
 
+import de.tonypsilon.bmm.backend.organization.data.OrganizationData;
 import de.tonypsilon.bmm.backend.organization.service.OrganizationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -58,12 +59,37 @@ class AuthorizationServiceTest {
 
     @Test
     void testVerifyUserIsClubAdminOfOrganizationSuccess() {
+        String username = "username";
+        Set<Long> clubIds = Set.of(1L, 2L);
+        when(clubAdminService.getAdminsOfClub(1L)).thenReturn(Set.of("some user", "another user"));
+        when(clubAdminService.getAdminsOfClub(2L)).thenReturn(Set.of("username", "some user"));
+        when(organizationService.getOrganizationById(3L))
+                .thenReturn(new OrganizationData(3L, 1L, "organization", Set.of(1L, 2L)));
 
+        ArgumentCaptor<Long> clubIdArgumentCaptor = ArgumentCaptor.forClass(Long.class);
+        authorizationService.verifyUserIsClubAdminOfOrganization(username, 3L);
+        verify(clubAdminService, times(2)).getAdminsOfClub(clubIdArgumentCaptor.capture());
+        verify(organizationService).getOrganizationById(3L);
+        assertThat(clubIdArgumentCaptor.getAllValues()).containsExactlyInAnyOrder(1L, 2L);
     }
 
     @Test
     void testVerifyUserIsClubAAdminOfOrganizationFailure() {
+        String username = "username";
+        Set<Long> clubIds = Set.of(1L, 2L);
+        when(clubAdminService.getAdminsOfClub(1L)).thenReturn(Set.of("some user", "another user"));
+        when(clubAdminService.getAdminsOfClub(2L)).thenReturn(Set.of("username2", "some user"));
+        when(organizationService.getOrganizationById(3L))
+                .thenReturn(new OrganizationData(3L, 1L, "organization", Set.of(1L, 2L)));
 
+        ArgumentCaptor<Long> clubIdArgumentCaptor = ArgumentCaptor.forClass(Long.class);
+        AccessDeniedException actualException = assertThrows(AccessDeniedException.class,
+                () -> authorizationService.verifyUserIsClubAdminOfOrganization(username, 3L));
+        verify(clubAdminService, times(2)).getAdminsOfClub(clubIdArgumentCaptor.capture());
+        verify(organizationService).getOrganizationById(3L);
+        assertThat(clubIdArgumentCaptor.getAllValues()).containsExactlyInAnyOrder(1L, 2L);
+        assertThat(actualException.getMessage())
+                .isEqualTo("username hat nicht ausreichend Vereinsadministrationsrechte!");
     }
 
     @Test
