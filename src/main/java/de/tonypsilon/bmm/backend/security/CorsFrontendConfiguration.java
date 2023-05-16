208a8fb3a75c1e99a2062c9ab99cb260957bc8ba
Tonypsilon <tony.schwedek@posteo.de>
2023-05-16 07:27:10 +0200

    Improve cors configuration.

--- src/main/java/de/tonypsilon/bmm/backend/security/CorsFrontendConfiguration.java
+++ src/main/java/de/tonypsilon/bmm/backend/security/CorsFrontendConfiguration.java
@@ -3,41 +3,31 @@ package de.tonypsilon.bmm.backend.security;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvc
 @ConfigurationProperties(prefix = "de.tonypsilon.bmm.cors.frontend")
 public class CorsFrontendConfiguration {
 
     private String url;
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        System.out.println(url);
-        configuration.setAllowedOrigins(List.of(url));
-        configuration.setAllowedMethods(List.of("GET","POST","OPTIONS","PATCH","DELETE"));
-        configuration.setAllowCredentials(Boolean.TRUE);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
-    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(url);
+                registry.addMapping("/**")
+                        .allowedOrigins(url)
+                        .allowCredentials(true)
+                        .allowedMethods("GET","POST","OPTIONS","PATCH","DELETE");
                 System.out.println("corsConfigurer: " + url);
             }
         };
