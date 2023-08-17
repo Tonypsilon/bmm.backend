e7cb989551dcd237d1a084b08f78863cfd04452d
Tonypsilon <tony.schwedek@posteo.de>
2023-08-17 21:17:59 +0200

    #17 Provide organizations of user that belong to seasons in registration phase.

--- src/main/java/de/tonypsilon/bmm/backend/organization/facade/OrganizationController.java
+++ src/main/java/de/tonypsilon/bmm/backend/organization/facade/OrganizationController.java
@@ -1,27 +1,43 @@
 package de.tonypsilon.bmm.backend.organization.facade;
 
+import de.tonypsilon.bmm.backend.datatypes.IdAndLabel;
 import de.tonypsilon.bmm.backend.organization.data.OrganizationCreationData;
 import de.tonypsilon.bmm.backend.organization.data.OrganizationData;
+import de.tonypsilon.bmm.backend.organization.service.OrganizationAdminService;
 import de.tonypsilon.bmm.backend.organization.service.OrganizationService;
+import de.tonypsilon.bmm.backend.season.service.SeasonService;
+import de.tonypsilon.bmm.backend.season.service.SeasonStage;
 import de.tonypsilon.bmm.backend.security.rnr.Roles;
 import de.tonypsilon.bmm.backend.security.rnr.service.AuthorizationService;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @RestController
 public class OrganizationController {
 
     private final OrganizationService organizationService;
+    private final OrganizationAdminService organizationAdminService;
+    private final SeasonService seasonService;
     private final AuthorizationService authorizationService;
 
     public OrganizationController(final OrganizationService organizationService,
+                                  final OrganizationAdminService organizationAdminService,
+                                  final SeasonService seasonService,
                                   final AuthorizationService authorizationService) {
         this.organizationService = organizationService;
+        this.organizationAdminService = organizationAdminService;
+        this.seasonService = seasonService;
         this.authorizationService = authorizationService;
     }
 
@@ -40,6 +56,18 @@ public class OrganizationController {
                 .body(organizationService.createOrganization(organizationCreationData));
     }
 
-
+    @RolesAllowed(Roles.CLUB_ADMIN)
+    @GetMapping(value = "/organizations/registration", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<IdAndLabel>> getOrganizationsInRegistrationStageForUser(Principal principal) {
+        return ResponseEntity.ok(
+                organizationAdminService.getOrganizationsOfUser(principal.getName())
+                        .stream()
+                        .filter(organizationData -> seasonService.getStageOfSeason(organizationData.seasonId())
+                                == SeasonStage.REGISTRATION)
+                        .map(organizationData ->
+                                new IdAndLabel(organizationData.id(), organizationData.name()))
+                        .toList()
+        );
+    }
 
 }
---
+++ src/test/java/de/tonypsilon/bmm/backend/organization/facade/OrganizationControllerTest.java
@@ -0,0 +1,72 @@
+package de.tonypsilon.bmm.backend.organization.facade;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.tonypsilon.bmm.backend.datatypes.IdAndLabel;
+import de.tonypsilon.bmm.backend.organization.data.OrganizationData;
+import de.tonypsilon.bmm.backend.organization.service.OrganizationAdminService;
+import de.tonypsilon.bmm.backend.organization.service.OrganizationService;
+import de.tonypsilon.bmm.backend.season.service.SeasonService;
+import de.tonypsilon.bmm.backend.season.service.SeasonStage;
+import de.tonypsilon.bmm.backend.security.rnr.Roles;
+import de.tonypsilon.bmm.backend.security.rnr.service.AuthorizationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(OrganizationController.class)
+class OrganizationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrganizationService organizationService;
+
+    @MockBean
+    private OrganizationAdminService organizationAdminService;
+
+    @MockBean
+    private SeasonService seasonService;
+
+    @MockBean
+    private AuthorizationService authorizationService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String username = "username";
+
+    @Test
+    @WithMockUser(username = username, authorities = {Roles.CLUB_ADMIN})
+    void testGetOrganizationsInRegistrationStageForUser() throws Exception {
+        OrganizationData organizationWrongSeasonStage =
+                new OrganizationData(1L, 2L, "wrong", Set.of(4L));
+        OrganizationData organizationRegistrationStage =
+                new OrganizationData(2L, 3L, "registration", Set.of(4L));
+        when(organizationAdminService.getOrganizationsOfUser(username))
+                .thenReturn(Set.of(organizationRegistrationStage, organizationWrongSeasonStage));
+        when(seasonService.getStageOfSeason(2L)).thenReturn(SeasonStage.PREPARATION);
+        when(seasonService.getStageOfSeason(3L)).thenReturn(SeasonStage.REGISTRATION);
+        var actualResponse = mockMvc
+                .perform(get("/organizations/registration"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+        var actual = objectMapper.readValue(actualResponse.getContentAsString(),
+                new TypeReference<List<IdAndLabel>>() { });
+        assertThat(actual)
+                .hasSize(1)
+                .containsExactly(new IdAndLabel(2L, "registration"));
+    }
+
+}
