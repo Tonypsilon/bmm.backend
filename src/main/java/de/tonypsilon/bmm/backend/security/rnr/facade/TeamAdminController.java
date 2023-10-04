9fee5a3c056753e01d3e9732e4979beecbfc8dae
Tonypsilon <tony.schwedek@posteo.de>
2023-10-04 22:29:44 +0200

    #28 Provide get endpoint with information relevant to create new team admins.

--- src/main/java/de/tonypsilon/bmm/backend/security/rnr/facade/TeamAdminController.java
+++ src/main/java/de/tonypsilon/bmm/backend/security/rnr/facade/TeamAdminController.java
@@ -1,5 +1,10 @@
 package de.tonypsilon.bmm.backend.security.rnr.facade;
 
+import de.tonypsilon.bmm.backend.datatypes.IdAndLabel;
+import de.tonypsilon.bmm.backend.exception.SeasonStageException;
+import de.tonypsilon.bmm.backend.organization.service.OrganizationService;
+import de.tonypsilon.bmm.backend.season.service.SeasonService;
+import de.tonypsilon.bmm.backend.season.service.SeasonStage;
 import de.tonypsilon.bmm.backend.security.rnr.Roles;
 import de.tonypsilon.bmm.backend.security.rnr.data.TeamAdminData;
 import de.tonypsilon.bmm.backend.security.rnr.service.AuthorizationService;
@@ -9,13 +14,13 @@ import de.tonypsilon.bmm.backend.team.service.TeamService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @RestController
 public class TeamAdminController {
@@ -24,13 +29,19 @@ public class TeamAdminController {
     private final TeamAdminService teamAdminService;
     private final TeamService teamService;
     private final AuthorizationService authorizationService;
+    private final OrganizationService organizationService;
+    private final SeasonService seasonService;
 
     public TeamAdminController(final TeamAdminService teamAdminService,
                                final TeamService teamService,
-                               final AuthorizationService authorizationService) {
+                               final AuthorizationService authorizationService,
+                               final OrganizationService organizationService,
+                               final SeasonService seasonService) {
         this.teamAdminService = teamAdminService;
         this.teamService = teamService;
         this.authorizationService = authorizationService;
+        this.organizationService = organizationService;
+        this.seasonService = seasonService;
     }
 
     @RolesAllowed(Roles.CLUB_ADMIN)
@@ -64,6 +75,22 @@ public class TeamAdminController {
         authorizationService.verifyUserIsClubAdminOfOrganization(principal.getName(), teamData.organizationId());
         teamAdminService.deleteTeamAdmin(teamAdminData);
         return ResponseEntity.noContent().build();
+    }
+
+    @RolesAllowed(Roles.CLUB_ADMIN)
+    @GetMapping(value = "/organization/{organizationId}/teams", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<IdAndLabel>> getAvailableTeams(Principal principal,
+                                                              @PathVariable Long organizationId) {
+        authorizationService.verifyUserIsClubAdminOfOrganization(
+                principal.getName(), Objects.requireNonNull(organizationId));
+        if(!Set.of(SeasonStage.REGISTRATION, SeasonStage.PREPARATION, SeasonStage.RUNNING).contains(
+                seasonService.getStageOfSeason(organizationService.getSeasonIdOfOrganization(organizationId)))) {
+            throw new SeasonStageException("In dieser Saisonphase können keine Teamadmins betrachtet werden!");
+        }
+        return ResponseEntity
+                .ok(teamService.getTeamsOfOrganization(organizationId).stream()
+                        .map(teamData -> new IdAndLabel(teamData.id(), teamData.name()))
+                        .toList());
     }
 
 }
