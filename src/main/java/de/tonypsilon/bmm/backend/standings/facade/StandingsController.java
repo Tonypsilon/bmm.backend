94df5455c417d28ff9626bd954751fc657f99346
Tonypsilon <tony.schwedek@posteo.de>
2023-12-21 14:51:08 +0100

    #36 Implement controller call to StandingsAssembler.

--- src/main/java/de/tonypsilon/bmm/backend/standings/facade/StandingsController.java
+++ src/main/java/de/tonypsilon/bmm/backend/standings/facade/StandingsController.java
@@ -1,18 +1,31 @@
 package de.tonypsilon.bmm.backend.standings.facade;
 
 import de.tonypsilon.bmm.backend.standings.data.StandingsData;
+import de.tonypsilon.bmm.backend.standings.service.StandingsAssembler;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class StandingsController {
 
+    private final StandingsAssembler standingsAssembler;
+
+    public StandingsController(
+            final StandingsAssembler standingsAssembler
+    ) {
+        this.standingsAssembler = standingsAssembler;
+    }
+
     @GetMapping(path = "/divisions/{divisionId}/standings", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StandingsData> getStandingsForDivision(@PathVariable final Long divisionId) {
-        return null;
+        return ResponseEntity
+                .ok()
+                .body(standingsAssembler.assembleStandings(Objects.requireNonNull(divisionId)));
     }
 
 }
